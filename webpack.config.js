f366a7ab096e670765dc1b49de4f4369f3f2aadf
Adam Eilers <adam.eilers@gmail.com>
2019-12-03 09:16:25 -0600

    numerous improvements to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
 const path = require('path')
 
-const Dotenv = require('dotenv-webpack')
 const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
+const Dotenv = require('dotenv-webpack')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const NodemonPlugin = require('nodemon-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = [
   // client-side config
   {
+    name: '- Web App',
+    bail: !isDev,
+    stats: {
+      all: false,
+      assets: true,
+      builtAt: true,
+      timings: true
+    },
     mode: process.env.NODE_ENV,
     entry: './src/client/index.jsx',
     output: {
       filename: '[name].js',
       path: path.resolve(__dirname, 'dist/client'),
+      pathinfo: false,
       publicPath: ''
     },
     target: 'web',
@@ -23,12 +35,33 @@ module.exports = [
           test: /\.js.?$/,
           include: /src/,
           exclude: /node_modules/,
-          use: ['babel-loader']
+          use: 'babel-loader?cacheDirectory'
         }
       ]
     },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: true,
+            ie8: false,
+            keep_classnames: false,
+            keep_fnames: false,
+            mangle: true,
+            module: false,
+            nameCache: null,
+            safari10: true,
+            sourceMap: true,
+            topLevel: false,
+            warnings: false
+          }
+        })
+      ]
+    },
     plugins: [
-      new Dotenv()
+      new Dotenv(),
+      new ManifestPlugin()
     ],
     resolve: {
       modules: [
@@ -38,16 +71,25 @@ module.exports = [
       ],
       extensions: ['.jsx', '.js', '.json']
     },
-    devtool: isDev ? 'inline-source-map' : 'nosources-source-map',
-    watch: false
+    devtool: isDev ? 'cheap-module-eval-source-map' : 'nosources-source-map',
+    watch: isDev
   },
   // server-side config
   {
+    name: '- SSR',
+    bail: !isDev,
+    stats: {
+      all: false,
+      assets: true,
+      builtAt: true,
+      timings: true
+    },
     mode: process.env.NODE_ENV,
     entry: './src/server/index.js',
     output: {
       filename: 'index.js',
-      path: path.resolve(__dirname, 'dist/server')
+      path: path.resolve(__dirname, 'dist/server'),
+      pathinfo: false
     },
     target: 'node',
     module: {
@@ -56,7 +98,7 @@ module.exports = [
           test: /\.js.?$/,
           include: /src/,
           exclude: /node_modules/,
-          use: ['babel-loader']
+          use: 'babel-loader?cacheDirectory'
         }, {
           test: /\.scss$/,
           use: [
@@ -82,6 +124,31 @@ module.exports = [
         }
       ]
     },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: true,
+            ie8: false,
+            keep_classnames: false,
+            keep_fnames: false,
+            mangle: true,
+            module: false,
+            nameCache: null,
+            safari10: false,
+            sourceMap: true,
+            topLevel: false,
+            warnings: false
+          }
+        })
+      ]
+    },
+    plugins: [
+      new NodemonPlugin({
+        script: './bin/start'
+      })
+    ],
     resolve: {
       modules: [
         path.resolve(__dirname, 'src/assets'),
@@ -90,6 +157,6 @@ module.exports = [
       ],
       extensions: ['.jsx', '.js', '.json']
     },
-    watch: false
+    watch: isDev
   }
 ]

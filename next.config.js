d0946fdde6e721fc17525fbc7645a37f2091fd0c
Adam Eilers <adam.eilers@gmail.com>
2023-04-03 09:19:51 -0500

    chore: updating project dependencies and settings for Next.js PWA

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: false
+})
+
+module.exports = withPWA({
+  experimental: {
+    appDir: true
+  },
+  output: 'export',
+  poweredByHeader: false,
+  reactStrictMode: true
+})
